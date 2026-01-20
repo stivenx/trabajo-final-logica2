1dae155da6d3a855b21fb51b0c0128ae40d9038e
stiven <stivenguti@gmail.com>
2026-01-20 11:44:53 -0500

    app

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -6,11 +6,18 @@ import categoryRoutes from "./routes/catrgory";
 import typeroutes from "./routes/typeRoutes";
 import carritoroutes from "./routes/carrito";
 import pruebaRoutes from "./routes/prueba";
+import comentariosRoutes from "./routes/comentarioRoutes";
+import documentsRoutes from "./routes/documentsRoutes";
 import path from "path";
+
 const app = Express();
  
 // Servir archivos estáticos desde la carpeta uploads
 app.use("/uploads", Express.static(path.join(__dirname, "../uploads")));
+app.use("/comentarios", Express.static(path.join(__dirname, "../comentarios")));
+app.use("/documents", Express.static(path.join(__dirname, "../documents")));
+app.use("/products", Express.static(path.join(__dirname, "../products")));
+
 
 app.use(cors());
 app.use(Express.json());
@@ -20,5 +27,7 @@ app.use("/api/categories", categoryRoutes);
 app.use("/api/types", typeroutes);
 app.use("/api/carts", carritoroutes);
 app.use("/api/prueba", pruebaRoutes);
+app.use("/api/comentarios", comentariosRoutes);
+app.use("/api/documents", documentsRoutes);
 
 export default app;
